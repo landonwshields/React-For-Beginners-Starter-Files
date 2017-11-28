c572078a6f86bd1de5ca5470bc71fa97b18cd6d2
Landon Shields <landonwshields@gmail.com>
2017-11-28 10:28:17 -0700

    Able to add to order

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {formatPrice} from '../helpers';
+
+class Fish extends React.Component{
+  render(){
+    //const { detaisl, index } = this.props; **same as below**
+    const details = this.props.details;
+    const index = this.props.index;
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    const isAvailable = details.status === 'available';
+    const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
+    return(
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button onClick={() => this.props.addToOrder(this.props.index)} disabled={!isAvailable}>{buttonText}</button>
+      </li>
+    )
+  }
+}
+
+
+export default Fish;
